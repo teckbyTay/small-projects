689bbcdc2cf7d22858b9f6835b8a4eda730489dc
Taylor M. Smith <117692454+teckbyTay@users.noreply.github.com>
2022-12-13 18:53:08 -0600

    ASCII block letter simple project

---
+++ ASCII_BlockLetters.py
@@ -0,0 +1,28 @@
+#Name: Taylor Smith
+#Fun fact: I love coding
+
+#Making first initial as a block letter
+
+first_initial = """
+
+T T T T T T
+    T
+    T
+    T
+    T
+
+"""
+second_initial = """
+
+   S S S
+S          S
+S
+   S S S   
+            S
+            S
+ S
+   S  S S           
+
+"""
+
+print(first_initial + second_initial)
